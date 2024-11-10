b2431a795ce23a4ae58e6e9ad29a3779b6716d73
vincecloutier <vincentcloutier@mail.com>
2024-11-10 10:36:44 +0100

    defaults

--- src/options.py
+++ src/options.py
@@ -5,7 +5,7 @@ def args_parser():
     parser = argparse.ArgumentParser()
 
     # federated arguments (Notation for the arguments followed from paper)
-    parser.add_argument('--epochs', type=int, default=25, help="number of rounds of training")
+    parser.add_argument('--epochs', type=int, default=10, help="number of rounds of training")
     parser.add_argument('--num_users', type=int, default=100, help="number of users: K")
     parser.add_argument('--frac', type=float, default=0.1, help='the fraction of clients: C')
     parser.add_argument('--local_ep', type=int, default=10, help="the number of local epochs: E")
@@ -22,9 +22,9 @@ def args_parser():
     parser.add_argument('--max_pool', type=str, default='True', help="Whether use max pooling rather than strided convolutions")
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='resnet', help="name of dataset")
+    parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
     parser.add_argument('--num_classes', type=int, default=10, help="number of classes")
-    parser.add_argument('--optimizer', type=str, default='adam', help="type of optimizer")
+    parser.add_argument('--optimizer', type=str, default='sgd', help="type of optimizer")
     parser.add_argument('--iid', type=int, default=1, help='Default set to IID. Set to 0 for non-IID.')
     parser.add_argument('--unequal', type=int, default=0, help='whether to use unequal data splits for non-i.i.d setting (use 0 for equal splits)')
     parser.add_argument('--stopping_rounds', type=int, default=10, help='rounds of early stopping')
