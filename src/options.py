d6d786cd46a687f52e4a94a21ae7b4b71ce22125
vincecloutier <vincentcloutier@mail.com>
2024-11-12 16:07:49 +0100

    changed to iid

--- src/options.py
+++ src/options.py
@@ -16,7 +16,7 @@ def args_parser():
     # other arguments
     parser.add_argument('--dataset', type=str, default='mobilenet', help="name of dataset")
     parser.add_argument('--optimizer', type=str, default='adam', help="type of optimizer")
-    parser.add_argument('--setting', type=int, default=2, help= "Set to 0 for IID, 1 for non-iid, 2 for mislabeled.")
+    parser.add_argument('--setting', type=int, default=0, help= "Set to 0 for IID, 1 for non-iid, 2 for mislabeled.")
     parser.add_argument('--badclient_prop', type=float, default=0.3, help= "Proportion of either non-iid or mislabeled or noisy clients.")
     parser.add_argument('--num_categories_per_client', type=int, default=4, help= "Number of categories per client in non-iid setting.")
     parser.add_argument('--mislabel_proportion', type=float, default=0.4, help= "Proportion of mislabeled samples per client in mislabeled setting.")
