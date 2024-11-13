80d602ad829d00dfa019f201b21994c0e7a423cf
vincecloutier <vincentcloutier@mail.com>
2024-11-13 12:16:45 +0100

    testing faster option

--- src/options.py
+++ src/options.py
@@ -5,8 +5,8 @@ def args_parser():
     parser = argparse.ArgumentParser()
 
     # federated arguments
-    parser.add_argument('--epochs', type=int, default=1, help="number of rounds of training")
-    parser.add_argument('--num_users', type=int, default=2, help="number of users: K")
+    parser.add_argument('--epochs', type=int, default=10, help="number of rounds of training")
+    parser.add_argument('--num_users', type=int, default=5, help="number of users: K")
     parser.add_argument('--frac', type=float, default=0.6, help='the fraction of clients: C')
     parser.add_argument('--local_ep', type=int, default=3, help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=64, help="local batch size: B")
@@ -14,7 +14,7 @@ def args_parser():
     parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum (default: 0.5)')
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='mobilenet', help="name of dataset")
+    parser.add_argument('--dataset', type=str, default='cifar', help="name of dataset")
     parser.add_argument('--optimizer', type=str, default='sgd', help="type of optimizer")
     parser.add_argument('--setting', type=int, default=0, help= "Set to 0 for IID, 1 for non-iid, 2 for mislabeled.")
     parser.add_argument('--badclient_prop', type=float, default=0.3, help= "Proportion of either non-iid or mislabeled or noisy clients.")
