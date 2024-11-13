7a717f69b9280f0bb650c6c1f53c93b8edbcf495
vincecloutier <vincentcloutier@mail.com>
2024-11-13 18:33:18 +0100

    logging w/ momentum

--- src/main.py
+++ src/main.py
@@ -117,7 +117,8 @@ if __name__ == '__main__':
             setting_str = f"{len(actual_bad_clients)} Bad Clients" + f" with {args.num_categories_per_client} Categories Per Bad Client"
         case 2:
             setting_str = f"{len(actual_bad_clients)} Bad Clients" + f" with {100*args.mislabel_proportion}% Mislabeled Samples Per Bad Client"
-    logger.info(f'Number Of Clients: {args.num_users}, Client Selection Fraction: {args.frac}, Local Epochs: {args.local_ep}, Batch Size: {args.local_bs}')
+    logger.info(f'Number Of Clients: {args.num_users}, Client Selection Fraction: {args.frac}, Local Epochs: {args.local_ep}')
+    logger.info(f'Batch Size: {args.local_bs}, Learning Rate: {args.lr}, Momentum: {args.momentum}')
     logger.info(f'Convergence Round: {convergence_round}, Retraining Convergence Round: {second_convergence_round}, Number Of Rounds: {args.epochs}')
     logger.info(f'Dataset: {args.dataset}, Setting: {setting_str}')
     logger.info(f'Test Accuracy Before Retraining: {100*test_acc}%')
