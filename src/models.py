290e7a2828b5ac3a29311cee51e4300eedf3c161
vincecloutier <vincentcloutier@mail.com>
2024-11-16 13:10:46 +0100

    fashion mnist

--- src/models.py
+++ src/models.py
@@ -1,26 +1,6 @@
 from torch import nn
 import torch.nn.functional as F
 
-class CNNMnist(nn.Module):
-    def __init__(self, args):
-        super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2), inplace=False)
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2), inplace=False)
-        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
-        x = F.relu(self.fc1(x), inplace=False)
-        x = F.dropout(x, training=self.training, inplace=False)
-        x = self.fc2(x)
-        # return F.log_softmax(x, dim=1)
-        return x # for cross entropy loss
-
-
 class CNNFashion_Mnist(nn.Module):
     def __init__(self, args):
         super(CNNFashion_Mnist, self).__init__()
--- src/options.py
+++ src/options.py
@@ -10,8 +10,8 @@ def args_parser():
     parser.add_argument('--frac', type=float, default=0.6, help='the fraction of clients: C')
     parser.add_argument('--hessian', type=int, default=0, help='Use Hessian-vector product to compute Banzhaf values (default: 0)')
 
-    parser.add_argument('--dataset', type=str, default='cifar', help="name of dataset")
-    parser.add_argument('--setting', type=int, default=1, help= "Set to 0 for IID, 1 for non-iid, 2 for mislabeled, 3 for noisy.")
+    parser.add_argument('--dataset', type=str, default='fmnist', help="name of dataset")
+    parser.add_argument('--setting', type=int, default=0, help= "Set to 0 for IID, 1 for non-iid, 2 for mislabeled, 3 for noisy.")
     parser.add_argument('--badclient_prop', type=float, default=0.3, help= "Proportion of either non-iid or mislabeled or noisy clients.")
     parser.add_argument('--num_categories_per_client', type=int, default=4, help= "Number of categories per client in non-iid setting.")
     parser.add_argument('--mislabel_proportion', type=float, default=0.4, help= "Proportion of mislabeled samples per client in mislabeled setting.")
