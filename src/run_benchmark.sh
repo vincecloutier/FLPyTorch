89858979dfd2bd30fc71bc505c3b4d750b1349f0
vincecloutier <vincentcloutier@mail.com>
2024-11-17 16:09:42 +0100

    fmnist only

--- src/run_benchmark.sh
+++ src/run_benchmark.sh
@@ -11,13 +11,13 @@ do
     python benchmark.py --dataset fmnist --setting 2 --processes 30 --local_ep 3
     python benchmark.py --dataset fmnist --setting 3 --processes 30 --local_ep 3
 
-    echo "Run $i for IMAGENET"
+    # echo "Run $i for IMAGENET"
 
-    # IMAGENET commands
-    python benchmark.py --dataset imagenet --setting 0 --processes 30 --local_ep 10
-    python benchmark.py --dataset imagenet --setting 1 --processes 30 --local_ep 3
-    python benchmark.py --dataset imagenet --setting 2 --processes 30 --local_ep 3
-    python benchmark.py --dataset imagenet --setting 3 --processes 30 --local_ep 3
+    # # IMAGENET commands
+    # python benchmark.py --dataset imagenet --setting 0 --processes 30 --local_ep 10
+    # python benchmark.py --dataset imagenet --setting 1 --processes 30 --local_ep 3
+    # python benchmark.py --dataset imagenet --setting 2 --processes 30 --local_ep 3
+    # python benchmark.py --dataset imagenet --setting 3 --processes 30 --local_ep 3
 
 done
 
