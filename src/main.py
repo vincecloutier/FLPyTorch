ed6e70dd03bcc67ea3587aff596666c0ec6df438
vincecloutier <vincentcloutier@mail.com>
2024-11-17 14:15:55 +0100

    alloc config

--- src/main.py
+++ src/main.py
@@ -12,7 +12,9 @@ import warnings
 import multiprocessing
 from functools import partial
 warnings.filterwarnings("ignore", category=UserWarning)
+import os
 
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
 
 def train_client(idx, args, global_weights, train_dataset, user_groups, epoch, device):
     torch.cuda.set_device(device)
