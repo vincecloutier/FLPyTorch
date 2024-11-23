4f3649880e4cee4af3e56d41dc9f8949c9eec2e3
vincecloutier <vincentcloutier@mail.com>
2024-11-23 14:27:09 +0100

    test

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,11 +1,9 @@
 import itertools
 from math import factorial as fact
 from collections import defaultdict
-import copy
 import time
 import torch
 import numpy as np
-from tqdm import tqdm
 from options import args_parser
 from update import inference, gradient
 from torch.utils.data import DataLoader
@@ -14,12 +12,11 @@ from utils import average_weights, setup_logger, get_device, identify_bad_idxs, 
 from sampling import get_dataset, SubsetSplit
 from scipy.stats import pearsonr
 import ray
-from ray.util.iter import from_items
+import copy
 from torch import nn
 
-@ray.remote(num_gpus=0.2)
-def train_subset(args, train_loaders, valid_dataset, test_dataset, clients=None):
-    model = initialize_model(args)
+@ray.remote(num_gpus=0.5)
+def train_subset(args, global_model, train_loaders, valid_dataset, test_dataset, global_weights, clients=None):
     subset_key = tuple(sorted(clients))
     isBanzhaf = (subset_key == (0, 1, 2, 3, 4))
     device = get_device()
@@ -27,7 +24,8 @@ def train_subset(args, train_loaders, valid_dataset, test_dataset, clients=None)
     if clients is None or len(clients) == 0:
         return subset_key, float('inf'), 0, defaultdict(float), defaultdict(float)
 
-    global_weights = model.state_dict()
+    global_model.load_state_dict(global_weights)
+    model = global_model.to(device)
     best_test_acc, best_test_loss = 0, float('inf')
     approx_banzhaf_values_hessian = defaultdict(float)
     approx_banzhaf_values_simple = defaultdict(float)
@@ -40,15 +38,15 @@ def train_subset(args, train_loaders, valid_dataset, test_dataset, clients=None)
         local_weights = []
         model.train()
         if isBanzhaf:
-            global_gradient = gradient(args, model, valid_dataset)
+            grad = gradient(args, model, valid_dataset)
 
         m = max(int(args.frac * len(clients)), 1)
         idxs_users = np.random.choice(clients, m, replace=False)
 
-        client_futures = [train_client.remote(args, copy.deepcopy(model), train_loaders[idx], device) for idx in idxs_users]
+        client_futures = [train_client.remote(args, global_weights, train_loaders[idx], device) for idx in idxs_users]
         client_results = ray.get(client_futures)
 
-        for w in client_results:
+        for idx, w in zip(idxs_users, client_results):
             local_weights.append(copy.deepcopy(w))
             if isBanzhaf:
                 delta_t[epoch][idx] = {key: (global_weights[key] - w[key]) for key in w.keys()}
@@ -60,8 +58,8 @@ def train_subset(args, train_loaders, valid_dataset, test_dataset, clients=None)
                 if epoch > 0:
                     for key in global_weights.keys():
                         delta_g[idx][key] += G_t_minus_i[key] - G_t[key]
-                approx_banzhaf_values_hessian[idx] += compute_bv_hvp(args, model, test_dataset, global_gradient, delta_t[epoch][idx], delta_g[idx], device)
-                approx_banzhaf_values_simple[idx] += compute_bv_simple(args, global_gradient, delta_t[epoch][idx])
+                approx_banzhaf_values_hessian[idx] += compute_bv_hvp(args, model, test_dataset, grad, delta_t[epoch][idx], delta_g[idx], device)
+                approx_banzhaf_values_simple[idx] += compute_bv_simple(args, grad, delta_t[epoch][idx])
 
         global_weights = average_weights(local_weights)
         model.load_state_dict(global_weights)
@@ -79,10 +77,11 @@ def train_subset(args, train_loaders, valid_dataset, test_dataset, clients=None)
     torch.cuda.empty_cache()
     return subset_key, best_test_loss, best_test_acc, approx_banzhaf_values_simple, approx_banzhaf_values_hessian
 
-
-@ray.remote(num_gpus=0.1)
-def train_client(args, model, trainloader, device):
-    # model.load_state_dict(global_weights)
+@ray.remote(num_gpus=0.25)
+def train_client(args, global_weights, trainloader, device):
+    # create a model and load global weights
+    model = initialize_model(args).to(device)
+    model.load_state_dict(global_weights)
 
     # set model to train
     model.train()
@@ -96,12 +95,12 @@ def train_client(args, model, trainloader, device):
     elif args.optimizer == 'adam':
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
 
-    for iter in range(args.local_ep):
+    for _ in range(args.local_ep):
         batch_loss = []
         for images, labels in trainloader:
             images, labels = images.to(device), labels.to(device)
 
-            model.zero_grad()
+            optimizer.zero_grad()
             log_probs = model(images)
             loss = criterion(log_probs, labels)
             loss.backward()
@@ -110,9 +109,7 @@ def train_client(args, model, trainloader, device):
 
             optimizer.step()
             batch_loss.append(loss.item())
-        epoch_loss.append(sum(batch_loss)/len(batch_loss))
-        if args.verbose:
-            print(f'| Local Epoch : {iter+1} | Loss: {sum(batch_loss) / len(batch_loss):.6f}')
+        epoch_loss.append(sum(batch_loss) / len(batch_loss))
 
     return model.state_dict()
 
@@ -120,28 +117,42 @@ def train_client(args, model, trainloader, device):
 if __name__ == '__main__':
     ray.init(include_dashboard=True, logging_level="DEBUG", object_store_memory=20 * 1024**3)
     start_time = time.time()
+
     args = args_parser()
     logger = setup_logger(f'benchmark_{args.dataset}_{args.setting}')
 
     train_dataset, valid_dataset, test_dataset, user_groups, actual_bad_clients = get_dataset(args)
 
-    all_subsets = list(itertools.chain.from_iterable(itertools.combinations(range(args.num_users), r) for r in range(args.num_users + 1)))
+    global_model = initialize_model(args)
+    global_weights = global_model.state_dict()
 
-    train_dataset_ref = ray.put(train_dataset)
-    valid_dataset_ref = ray.put(valid_dataset)
-    test_dataset_ref = ray.put(test_dataset)
-    user_groups_ref = ray.put(user_groups)
-
-    train_loaders = {}
-    for user_id, indices in user_groups.items():
-        train_loader = DataLoader(SubsetSplit(train_dataset, indices), batch_size=args.local_bs, shuffle=True, num_workers=args.num_workers)
-        train_loaders[user_id] = train_loader
+    train_loaders = {
+        user_id: DataLoader(
+            SubsetSplit(train_dataset, indices),
+            batch_size=args.local_bs,
+            shuffle=True,
+            num_workers=args.num_workers
+        )
+        for user_id, indices in user_groups.items()
+    }
 
     train_loaders_ref = ray.put(train_loaders)
+    valid_dataset_ref = ray.put(valid_dataset)
+    test_dataset_ref = ray.put(test_dataset)
 
-    futures = [train_subset.remote(args, train_loaders_ref, valid_dataset_ref, test_dataset_ref, clients) for clients in all_subsets]
+    all_subsets = list(itertools.chain.from_iterable(itertools.combinations(range(args.num_users), r) for r in range(args.num_users + 1)))
+
+    futures = [
+        train_subset.remote(args, global_model, train_loaders_ref, valid_dataset_ref, test_dataset_ref, global_weights, clients)
+        for clients in all_subsets
+    ]
+
     results_list = ray.get(futures)
-    results = {subset_key: (loss, accuracy, abv_simple, abv_hessian) for subset_key, loss, accuracy, abv_simple, abv_hessian in from_items(results_list)}
+    results = {
+        subset_key: (loss, accuracy, abv_simple, abv_hessian)
+        for subset_key, loss, accuracy, abv_simple, abv_hessian in results_list
+    }
+
     shapley_values, banzhaf_values = defaultdict(float), defaultdict(float)
     for client in range(args.num_users):
         for r in range(args.num_users):
@@ -152,41 +163,4 @@ if __name__ == '__main__':
                 shapley_values[client] += ((fact(len(subset)) * fact(args.num_users - len(subset) - 1)) / fact(args.num_users)) * mc
                 banzhaf_values[client] += mc / len(all_subsets)
 
-    longest_client_key = max(results.keys(), key=len)
-    test_loss, test_acc, abv_simple, abv_hessian = results[longest_client_key]
-
-    identified_bad_clients_simple = identify_bad_idxs(abv_simple)
-    identified_bad_clients_hessian = identify_bad_idxs(abv_hessian)
-    bad_client_accuracy_simple = measure_accuracy(actual_bad_clients, identified_bad_clients_simple)
-    bad_client_accuracy_hessian = measure_accuracy(actual_bad_clients, identified_bad_clients_hessian)
-
-    shared_clients = set(shapley_values.keys()) & set(banzhaf_values.keys()) & set(abv_simple.keys()) & set(abv_hessian.keys())
-    sv = [shapley_values[client] for client in shared_clients]
-    bv = [banzhaf_values[client] for client in shared_clients]
-    abv_simple = [abv_simple[client] for client in shared_clients]
-    abv_hessian = [abv_hessian[client] for client in shared_clients]
-
-    setting_str = {
-        0: "IID",
-        1: f"Non IID with {len(actual_bad_clients)} Bad Clients and {args.num_categories_per_client} Categories Per Bad Client",
-        2: f"Mislabeled with {len(actual_bad_clients)} Bad Clients and {100 * args.badsample_prop}% Bad Samples Per Bad Client",
-        3: f"Noisy with {len(actual_bad_clients)} Bad Clients and {100 * args.badsample_prop}% Bad Samples Per Bad Client"
-    }.get(args.setting, "Unknown Setting")
-    logger.info(f'Number Of Clients: {args.num_users}, Client Selection Fraction: {args.frac}, Local Epochs: {args.local_ep}, Batch Size: {args.local_bs}')
-    logger.info(f'Dataset: {args.dataset}, Setting: {setting_str}, Number Of Rounds: {args.epochs}')
-    logger.info(f'Test Accuracy Of Global Model: {100 * test_acc}%')
-    logger.info(f'Shapley Values: {sv}')
-    logger.info(f'Banzhaf Values: {bv}')
-    logger.info(f'Approximate Banzhaf Values Simple: {abv_simple}')
-    logger.info(f'Approximate Banzhaf Values Hessian: {abv_hessian}')
-    logger.info(f'Pearson Correlation Between Shapley And Banzhaf Values: {pearsonr(sv, bv)}')
-    logger.info(f'Pearson Correlation Between Shapley And Approximate Banzhaf Values Simple: {pearsonr(sv, abv_simple)}')
-    logger.info(f'Pearson Correlation Between Shapley And Approximate Banzhaf Values Hessian: {pearsonr(sv, abv_hessian)}')
-    logger.info(f'Pearson Correlation Between Banzhaf And Approximate Banzhaf Values Simple: {pearsonr(bv, abv_simple)}')
-    logger.info(f'Pearson Correlation Between Banzhaf And Approximate Banzhaf Values Hessian: {pearsonr(bv, abv_hessian)}')
-    logger.info(f'Actual Bad Clients: {actual_bad_clients}')
-    logger.info(f'Identified Bad Clients Simple: {identified_bad_clients_simple}')
-    logger.info(f'Identified Bad Clients Hessian: {identified_bad_clients_hessian}')
-    logger.info(f'Bad Client Accuracy Simple: {bad_client_accuracy_simple}')
-    logger.info(f'Bad Client Accuracy Hessian: {bad_client_accuracy_hessian}')
     logger.info(f'Total Run Time: {time.time() - start_time}')
