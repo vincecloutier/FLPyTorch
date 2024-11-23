c0f6071b90d6a2c028eb863475bb9d4f3d359839
vincecloutier <vincentcloutier@mail.com>
2024-11-23 08:24:14 +0100

    let benchmark take processes

--- src/run_benchmark.sh
+++ src/run_benchmark.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
+# get processes argument
+if [ -z "$1" ]; then
+    PROCESSES=8  # default is eight
+else
+    PROCESSES=$1
+fi
+
+echo "Using $PROCESSES processes."
+
 # loop to run each command three times
 for i in {1..3}
 do
 
     # CIFAR commands
     # echo "Run $i for CIFAR"
-    # python benchmark.py --dataset cifar --setting 0 --processes 30 --local_ep 10
-    # python benchmark.py --dataset cifar --setting 1 --processes 30 --local_ep 3
-    # python benchmark.py --dataset cifar --setting 2 --processes 30 --local_ep 3
-    # python benchmark.py --dataset cifar --setting 3 --processes 30 --local_ep 3
+    # python benchmark.py --dataset cifar --setting 0 --processes $PROCESSES --local_ep 10
+    # python benchmark.py --dataset cifar --setting 1 --processes $PROCESSES --local_ep 3
+    # python benchmark.py --dataset cifar --setting 2 --processes $PROCESSES --local_ep 3
+    # python benchmark.py --dataset cifar --setting 3 --processes $PROCESSES --local_ep 3
 
     # FMNIST commands
     echo "Run $i for FMNIST"
-    python benchmark.py --dataset fmnist --setting 0 --processes 30 --local_ep 10
-    python benchmark.py --dataset fmnist --setting 1 --processes 30 --local_ep 3
-    python benchmark.py --dataset fmnist --setting 2 --processes 30 --local_ep 3
-    python benchmark.py --dataset fmnist --setting 3 --processes 30 --local_ep 3
+    python benchmark.py --dataset fmnist --setting 0 --processes $PROCESSES --local_ep 10
+    python benchmark.py --dataset fmnist --setting 1 --processes $PROCESSES --local_ep 3
+    python benchmark.py --dataset fmnist --setting 2 --processes $PROCESSES --local_ep 3
+    python benchmark.py --dataset fmnist --setting 3 --processes $PROCESSES --local_ep 3
 
     # IMAGENET commands
     # echo "Run $i for IMAGENET"
-    # python benchmark.py --dataset imagenet --setting 0 --processes 30 --local_ep 10
-    # python benchmark.py --dataset imagenet --setting 1 --processes 30 --local_ep 3
-    # python benchmark.py --dataset imagenet --setting 2 --processes 30 --local_ep 3
-    # python benchmark.py --dataset imagenet --setting 3 --processes 30 --local_ep 3
+    # python benchmark.py --dataset imagenet --setting 0 --processes $PROCESSES --local_ep 10
+    # python benchmark.py --dataset imagenet --setting 1 --processes $PROCESSES --local_ep 3
+    # python benchmark.py --dataset imagenet --setting 2 --processes $PROCESSES --local_ep 3
+    # python benchmark.py --dataset imagenet --setting 3 --processes $PROCESSES --local_ep 3
 
 done
 
 echo "All runs completed."
 
 # chmod +x run_benchmark.sh
-# ./run_benchmark.sh
+# ./run_benchmark.sh [processes]
