e23f0d88819a9dc6a953e0c150d0d78324115cb3
vincecloutier <vincentcloutier@mail.com>
2024-11-30 12:07:55 +0100

    removed inplace

--- src/utils.py
+++ src/utils.py
@@ -55,7 +55,7 @@ def get_dataset(args):
             'test': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
         },
         'cifar': {
-            'train': transforms.Compose([transforms.RandomCrop(32, padding=4, padding_mode='reflect'), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010), inplace=True)]),
+            'train': transforms.Compose([transforms.RandomCrop(32, padding=4, padding_mode='reflect'), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]),
             'test': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]),
         },
         'imagenet': {
