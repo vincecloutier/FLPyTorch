016db8ad879b61cecf930e25b2299def815af3c2
vincecloutier <vincentcloutier@mail.com>
2024-12-01 16:27:47 +0100

    testing old version speed

--- src/valuation/shapley.py
+++ src/valuation/shapley.py
@@ -49,7 +49,9 @@ def compute_shapley(args, global_weights, client_weights, test_dataset):
         process_args = (client_keys, base_acc, device, args, num_permutations)
         args_list.append(process_args)
 
-    shapley_update_local = pool.map(handle_permutation, args_list)
+    args_list = [(client_keys, base_acc, device, args) for _ in range(t)]
+
+    shapley_update_local = pool.map(compute_shapley_for_permutation, args_list)
     pool.close()
     pool.join()
 
@@ -84,3 +86,27 @@ def handle_permutation(args):
             prev_acc = curr_acc
 
     return shapley_updates_local
+
+
+def compute_shapley_for_permutation(args):
+    client_keys, base_acc, device, args_model = args
+
+    model = initialize_model(args_model)
+    model.load_state_dict(_global_weights)
+    model.to(device)
+
+    shapley_updates_local = defaultdict(float)
+
+    permutation = np.random.permutation(client_keys)
+    prev_acc = base_acc
+    current_weights = []
+    for i in permutation:
+        current_weights.append(_client_weights[i])
+        avg_weights = average_weights(current_weights)
+        model.load_state_dict(avg_weights)
+        with torch.no_grad():
+            curr_acc = test_inference(model, _test_dataset)[0]
+        shapley_updates_local[i] += curr_acc - prev_acc
+        prev_acc = curr_acc
+    return shapley_updates_local
+
