bb733aafc97eafcbf77baf36f1fe89105bcfe703
vincecloutier <vincentcloutier@mail.com>
2024-12-01 13:38:09 +0100

    print statemetns

--- src/valuation/shapley.py
+++ src/valuation/shapley.py
@@ -6,29 +6,38 @@ def compute_shapley(args, global_weights, client_weights, test_dataset):
     """Estimate Shapley values for participants in a round using permutation sampling."""
     device = get_device()
     model = initialize_model(args) 
+    print("here1")
     model.load_state_dict(global_weights)
     model.to(device)
     model.train()
+    print("here2")
 
     m = len(client_weights)
     epsilon, delta, r = 0.1, 0.05, 1  # allow 10% error at 95% confidence, r = 1 since accuracy in [0, 1]  
     t = int((2 * r**2 / epsilon**2) * np.log(2 * m / delta))
     
+    print("here3")
     base_acc = test_inference(model, test_dataset)[0] 
     shapley_updates = np.zeros(m)
 
+    print("here4")
     for _ in range(t):
         permutation = np.random.permutation(m)
         prev_acc = base_acc
+        print("here5")
 
         model.load_state_dict(global_weights)
         current_weights = []
-
+        print("here6")
         for i in permutation:
             current_weights.append(client_weights[i])
+            print("here7")
             model.load_state_dict(average_weights(current_weights))
+            print("here8")
             curr_acc = test_inference(model, test_dataset)[0]
+            print("here9")
             shapley_updates[i] += curr_acc - prev_acc
+            print("here10")
             prev_acc = curr_acc
     
     shapley_updates /= t
