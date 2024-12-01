b6ef92740c4b73895c901f1d449faa6465485720
vincecloutier <vincentcloutier@mail.com>
2024-12-01 14:49:08 +0100

    tqdm to see progress

--- src/valuation/shapley.py
+++ src/valuation/shapley.py
@@ -2,6 +2,7 @@ import numpy as np
 from update import test_inference
 from utils import average_weights, initialize_model, get_device
 from collections import defaultdict
+from tqdm import tqdm
 
 def compute_shapley(args, global_weights, client_weights, test_dataset):
     """Estimate Shapley values for participants in a round using permutation sampling."""
@@ -19,18 +20,20 @@ def compute_shapley(args, global_weights, client_weights, test_dataset):
     base_acc = test_inference(model, test_dataset)[0] 
     shapley_updates = defaultdict(float)
 
-    for _ in range(t):
-        permutation = np.random.permutation(client_keys)
-        prev_acc = base_acc
-        model.load_state_dict(global_weights)
-        current_weights = []
-        for i in permutation:
-            current_weights.append(client_weights[i])
-            avg_weights = average_weights(current_weights)
-            model.load_state_dict(avg_weights)
-            curr_acc = test_inference(model, test_dataset)[0]
-            shapley_updates[i] += curr_acc - prev_acc
-            prev_acc = curr_acc
+    with tqdm(total=t) as pbar:
+        for i in range(t):
+            permutation = np.random.permutation(client_keys)
+            prev_acc = base_acc
+            model.load_state_dict(global_weights)
+            current_weights = []
+            for i in permutation:
+                current_weights.append(client_weights[i])
+                avg_weights = average_weights(current_weights)
+                model.load_state_dict(avg_weights)
+                curr_acc = test_inference(model, test_dataset)[0]
+                shapley_updates[i] += curr_acc - prev_acc
+                prev_acc = curr_acc
+            pbar.update(1)
     
     shapley_updates = {k: v / t for k, v in shapley_updates.items()}
     return shapley_updates
