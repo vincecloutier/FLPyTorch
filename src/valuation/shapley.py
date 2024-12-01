23a5ffd71af80e86b2d24ee0341cb4928b5d9b29
vincecloutier <vincentcloutier@mail.com>
2024-12-01 16:10:23 +0100

    trying something to speed it up

--- src/valuation/shapley.py
+++ src/valuation/shapley.py
@@ -4,6 +4,21 @@ from utils import average_weights, initialize_model, get_device
 from collections import defaultdict
 from tqdm import tqdm
 import multiprocessing
+import torch
+
+
+# global variables to avoid data copying
+_global_weights = None
+_client_weights = None
+_test_dataset = None
+
+
+def init_process(global_weights, client_weights, test_dataset):
+    global _global_weights, _client_weights, _test_dataset
+    _global_weights = global_weights
+    _client_weights = client_weights
+    _test_dataset = test_dataset
+
 
 def compute_shapley_for_permutations(args):
     (client_keys, client_weights, global_weights, base_acc, test_dataset, device, args_model, num_permutations) = args
@@ -11,19 +26,34 @@ def compute_shapley_for_permutations(args):
 
     model = initialize_model(args_model)
     model.load_state_dict(global_weights)
-    model.to(device) 
-    model.train()
+    model.to(device)
 
     for _ in tqdm(range(num_permutations), desc="Calculating Shapley Values"):
         permutation = np.random.permutation(client_keys)
         prev_acc = base_acc
 
-        current_weights = []
+        cumulative_weights_sum = {k: v.clone() for k, v in global_weights.items()}
+        n = 1  # start from 1 to include the global weights
+
         for i in permutation:
-            current_weights.append(client_weights[i])
-            avg_weights = average_weights(current_weights)
-            model.load_state_dict(avg_weights)
-            curr_acc = test_inference(model, test_dataset)[0]
+            client_weight = client_weights[i]
+
+            # incrementally update cumulative weights sum
+            for key in cumulative_weights_sum.keys():
+                cumulative_weights_sum[key] += client_weight[key]
+            n += 1
+
+            # compute average weights
+            avg_weights = {key: cumulative_weights_sum[key] / n for key in cumulative_weights_sum.keys()}
+
+            # update model parameters directly
+            with torch.no_grad():
+                for name, param in model.named_parameters():
+                    param.copy_(avg_weights[name])
+
+            # perform inference without tracking gradients
+            with torch.no_grad():
+                curr_acc = test_inference(model, test_dataset)[0]
             shapley_updates_local[i] += curr_acc - prev_acc
             prev_acc = curr_acc
 
@@ -33,34 +63,45 @@ def compute_shapley(args, global_weights, client_weights, test_dataset):
     """Estimate Shapley values for participants in a round using permutation sampling."""
     device = get_device()
 
-    # Initialize model and compute base accuracy
+    # initialize model and compute base accuracy
     model = initialize_model(args)
     model.load_state_dict(global_weights)
     model.to(device)
-    model.train()
-    base_acc = test_inference(model, test_dataset)[0]
+
+    with torch.no_grad():
+        base_acc = test_inference(model, test_dataset)[0]
 
     client_keys = list(client_weights.keys())
     m = len(client_keys)
-    epsilon, delta, r = 0.25, 0.25, 1  # Allow 25% error at 75% confidence
+    epsilon, delta, r = 0.25, 0.25, 1  # allow 25% error at 75% confidence
     t = int((2 * r**2 / epsilon**2) * np.log(2 * m / delta))
 
     shapley_updates = defaultdict(float)
   
     num_processes = args.shapley_processes
+
+    # avoid passing large data structures to each process
+    pool = multiprocessing.Pool(processes=num_processes, initializer=init_process, initargs=(global_weights, client_weights, test_dataset))
+
     args_list = []
     for i in range(num_processes):
         num_permutations = t // num_processes + (1 if i < (t % num_processes) else 0)
-        process_args = (client_keys, client_weights, global_weights, base_acc, test_dataset, device, args, num_permutations)
+        process_args = (client_keys, base_acc, device, args, num_permutations)
         args_list.append(process_args)
 
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        shapley_update_local = pool.map(compute_shapley_for_permutations, args_list)
+    shapley_update_local = pool.map(compute_shapley_worker, args_list)
+    pool.close()
+    pool.join()
 
     for updates in shapley_update_local:
         for k, v in updates.items():
             shapley_updates[k] += v
 
-    # Average the Shapley values over all permutations
+    # average the values over all permutations
     shapley_updates = {k: v / t for k, v in shapley_updates.items()}
     return shapley_updates
+
+
+def compute_shapley_worker(args):
+    client_keys, base_acc, device, args_model, num_permutations = args
+    return compute_shapley_for_permutations((client_keys, _client_weights, _global_weights, base_acc, _test_dataset, device, args_model, num_permutations))
