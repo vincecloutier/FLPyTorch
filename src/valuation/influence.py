c10b932de22800155bb24071518cd66452c79c18
vincecloutier <vincentcloutier@mail.com>
2024-12-02 13:12:23 +0100

    debugging

--- src/valuation/influence.py
+++ src/valuation/influence.py
@@ -55,12 +55,18 @@ def compute_client_influence(client_idx, args, model, train_dataset, user_groups
 def compute_influence(args, model, train_dataset, test_dataset, user_groups):
     """Estimate Influence values for participants in a round using permutation sampling."""
     influences = defaultdict(float)
-
+    print(f'Computing Influence Functions for {args.num_users} clients')
     for id, indexes in user_groups.items():
+        print(f'here')
         train_data_loader = DataLoader(ClientSplit(train_dataset, indexes), batch_size=args.local_bs, shuffle=False)
+        print(f'here2')
         influence_model = NystroemSketchInfluence(model, F.cross_entropy, rank = 30, hessian_regularization=0.1)
+        print(f'here3')
         influence_model = influence_model.fit(train_data_loader)
+        print(f'here4')
         all_influences = influence_model.influences(*test_dataset, *train_dataset, mode="up")
+        print(f'here5')
         influences[id] = sum(np.mean(all_influences.numpy(), axis=0))
+        print(f'here6')
 
     return influences
