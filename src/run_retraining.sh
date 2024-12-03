e18d8b433cc62118f22b0b4bb172a1666f7b9eaf
vincecloutier <vincentcloutier@mail.com>
2024-12-03 13:04:57 +0100

    bug

--- src/run_retraining.sh
+++ src/run_retraining.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get processes argument
-if [ --processes "$1" ]; then
+if [ -z "$1" ]; then
     PROCESSES=8  # default is eight
 else
-    PROCESSES=$1
+    PROCESSES="$1"
 fi
 
 echo "Using $PROCESSES processes."
@@ -16,9 +16,9 @@ do
     BAD_CLIENT_PROP=$(echo "scale=1; 0.2 * $i" | bc)
 
     echo "Run $i with bad_client_prop=$BAD_CLIENT_PROP"
-    python retraining.py --dataset resnet --setting 1 --processes $PROCESSES --bad_client_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
-    python retraining.py --dataset resnet --setting 2 --processes $PROCESSES --bad_client_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
-    python retraining.py --dataset resnet --setting 3 --processes $PROCESSES --bad_client_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
+    python retraining.py --dataset resnet --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
+    python retraining.py --dataset resnet --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
+    python retraining.py --dataset resnet --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1
 
 done
 
