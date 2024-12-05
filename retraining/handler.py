049aec072f598abc22a82a958ed08b286c88eb0f
vincecloutier <vincentcloutier@mail.com>
2024-12-05 12:12:14 +0100

    improving abv hessian identification helper

--- retraining/handler.py
+++ retraining/handler.py
@@ -5,6 +5,9 @@ from scipy.stats import spearmanr
 import numpy as np
 from collections import defaultdict
 import torch
+from sklearn.preprocessing import MinMaxScaler
+from mpmath import mp
+
 def process_log(file_path):
     # read the log file
     with open(file_path, 'r') as file:
@@ -13,7 +16,7 @@ def process_log(file_path):
     # regex patterns
     approx_simple_pattern = r"Banzhaf Values Simple: defaultdict\(<class 'float'>, \{(.*?)\}\)"
     approx_hessian_pattern = r"Banzhaf Values Hessian: defaultdict\(<class 'float'>, \{(.*?)\}\)"
-    actual_bad_clients_pattern = r"Actual Bad Clients: \[(.*?)\]"
+    bad_clients_pattern = r"Actual Bad Clients: \[([-\d\s,]+)\]"
     before_pattern = r"Test Accuracy Before Retraining: ([\d\.]+), Test Loss Before Retraining: ([\d\.]+)"
     after_pattern = r"Test Accuracy After Retraining: ([\d\.]+), Test Loss After Retraining: ([\d\.]+)"
     bad_client_accuracy_simple_pattern = r"Bad Client Accuracy Simple: (.*?)\n"
@@ -24,39 +27,43 @@ def process_log(file_path):
     approx_hessian_values = [eval(f"{{{match}}}") for match in re.findall(approx_hessian_pattern, logs)]
     before_values = [eval(f"{{{match}}}") for match in re.findall(before_pattern, logs)]
     after_values = [eval(f"{{{match}}}") for match in re.findall(after_pattern, logs)]
-    bad_client_accuracy_simple_values = [eval(f"{{{match}}}") for match in re.findall(bad_client_accuracy_simple_pattern, logs)]
-    bad_client_accuracy_hessian_values = [eval(f"{{{match}}}") for match in re.findall(bad_client_accuracy_hessian_pattern, logs)]
-    actual_bad_clients_values = [eval(f"{{{match}}}") for match in re.findall(actual_bad_clients_pattern, logs)]
+    bad_client_accuracy_simple_values = [float(match) for match in re.findall(bad_client_accuracy_simple_pattern, logs)]
+    bad_client_accuracy_hessian_values = [float(match) for match in re.findall(bad_client_accuracy_hessian_pattern, logs)]
+    all_bad_clients = [[int(num) for num in re.findall(r'-?\d+', match)] for match in re.findall(bad_clients_pattern, logs) if match]
 
     # print(approx_simple_values)
     # print(approx_hessian_values)
-    # print(before_values)
-    # print(after_values)
+    # # print(before_values)
+    # # print(after_values)
     # print(bad_client_accuracy_simple_values)
-    # print(bad_client_accuracy_hessian_values)
-    # print(actual_bad_clients_values)
+    # # print(bad_client_accuracy_hessian_values)
+    # print(all_bad_clients)
 
     new_bad_client_accuracy_simple_values = []
     new_bad_client_accuracy_hessian_values = []
-    for i in range(len(bad_client_accuracy_simple_values)):
-        bad_clients = actual_bad_clients_values[i]
-        abv_simple = approx_simple_values[i]
-        abv_hessian = approx_hessian_values[i]
-        new_bad_client_accuracy_simple_values.append(measure_accuracy(bad_clients, identify_bad_idxs(abv_simple)))
-        new_bad_client_accuracy_hessian_values.append(measure_accuracy(bad_clients, identify_bad_idxs(abv_hessian)))
-
+    for i in range(len(all_bad_clients)):
+        bca_s = measure_accuracy(all_bad_clients[i], identify_bad_idxs(approx_simple_values[i]))
+        bca_h = measure_accuracy(all_bad_clients[i], identify_bad_idxs(approx_hessian_values[i]))
+        new_bad_client_accuracy_simple_values.append(bca_s)
+        new_bad_client_accuracy_hessian_values.append(bca_h)
+     
     print(f"Simple: {bad_client_accuracy_simple_values} / {new_bad_client_accuracy_simple_values}")
     print(f"Hessian: {bad_client_accuracy_hessian_values} / {new_bad_client_accuracy_hessian_values}")
 
 
-
-def identify_bad_idxs(approx_banzhaf_values: dict, threshold: float = 3) -> list[int]:
+def identify_bad_idxs(approx_banzhaf_values: dict, threshold: float = 2) -> list[int]:
     if not approx_banzhaf_values:
         return []
-    banzhaf_tensor = torch.tensor(list(approx_banzhaf_values.values()))
-    median_banzhaf = torch.median(banzhaf_tensor)    
-    bad_idxs = [key for key, banzhaf in approx_banzhaf_values.items() if (banzhaf < median_banzhaf / threshold)]
-    # bad_idxs = [key for key, banzhaf in approx_banzhaf_values.items() if banzhaf < 0]
+    # banzhaf_tensor = torch.tensor(list(approx_banzhaf_values.values()))
+    # median_banzhaf = np.median(list(approx_banzhaf_values.values()))    
+    # add all negative values to the list
+    bad_idxs = [key for key, banzhaf in approx_banzhaf_values.items() if banzhaf < 0]
+
+    # also if the mean is less than the threshold, add all values less than the mean to the list
+    # approx_banzhaf_values = {k: v for k, v in approx_banzhaf_values.items() if k not in bad_idxs}
+    avg_banzhaf = np.mean(list(approx_banzhaf_values.values()))
+    med_banzhaf = np.median(list(approx_banzhaf_values.values()))
+    bad_idxs.extend([key for key, banzhaf in approx_banzhaf_values.items() if banzhaf < avg_banzhaf / threshold])
     return bad_idxs
 
 
@@ -74,7 +81,7 @@ def measure_accuracy(targets, predictions):
     return (TP + TN) / (TP + TN + FP + FN)
 
 
-process_log('retraining/cifar1.log')
+process_log('retraining/cifar2.log')
 
 
 
