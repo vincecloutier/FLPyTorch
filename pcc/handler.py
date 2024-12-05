5e20b8da74de8af418ab95ffea5868d4632db9b8
vincecloutier <vincentcloutier@mail.com>
2024-12-05 09:37:11 +0100

    cifar

--- pcc/handler.py
+++ pcc/handler.py
@@ -90,5 +90,3 @@ def process_and_graph_log(file_path, plot=False):
         plt.show()
 
 process_and_graph_log('pcc/cifar.log', plot=True)
-# process_and_graph_log('pcc/fmnist.log', plot=True)
-# process_and_graph_log('pcc/fmnist2.log', plot=True)
