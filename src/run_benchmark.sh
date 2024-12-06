dcc41f6879a972bf54ca0d7b33154179f232a3c0
vincecloutier <vincentcloutier@mail.com>
2024-12-06 09:55:34 +0100

    added accuracy stopping

--- src/run_benchmark.sh
+++ src/run_benchmark.sh
@@ -37,17 +37,19 @@ echo "Using $PROCESSES processes."
 echo "Number of runs: $RUNS"
 echo "Datasets: $DATASETS"
 
-# Loop for the number of runs
 for i in $(seq 1 $RUNS)
 do
-    # Split datasets by comma and loop through each
     for dataset in $(echo $DATASETS | tr ',' ' ')
     do
         echo "Run $i for $dataset"
-        # Run commands for settings 0 to 3
         for setting in {0..3}
         do
-            python benchmark.py --dataset $dataset --setting $setting --processes $PROCESSES
+            if [ $dataset == "fmnist" ]; then
+                ACCURACY_STOPPING=0
+            else
+                ACCURACY_STOPPING=1
+            fi
+            python benchmark.py --dataset $dataset --setting $setting --processes $PROCESSES --acc_stopping $ACCURACY_STOPPING
         done
     done
 done
@@ -55,4 +57,4 @@ done
 echo "All runs completed."
 
 # chmod +x run_benchmark.sh
-# ./run_benchmark.sh --processes 8 --runs 3 --datasets cifar,fmnist
+# ./run_benchmark.sh --processes 8 --datasets cifar,fmnist
