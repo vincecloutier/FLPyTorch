cd9692c3010e452a82279f6e297696808c5b9648
vincecloutier <vincentcloutier@mail.com>
2024-12-06 10:21:56 +0100

    upped it to 10 users

--- src/run_robustness.sh
+++ src/run_robustness.sh
@@ -9,10 +9,10 @@ fi
 
 echo "Using $PROCESSES processes."
 
-python robustness.py --dataset resnet --setting 0 --processes $PROCESSES --num_users 5 --local_ep 10
-python robustness.py --dataset resnet --setting 1 --processes $PROCESSES --num_users 5 --local_ep 10
-python robustness.py --dataset resnet --setting 2 --processes $PROCESSES --num_users 5 --local_ep 10
-python robustness.py --dataset resnet --setting 3 --processes $PROCESSES --num_users 5 --local_ep 10
+python robustness.py --dataset resnet --setting 0 --processes $PROCESSES --num_users 10 --local_ep 10
+python robustness.py --dataset resnet --setting 1 --processes $PROCESSES --num_users 10 --local_ep 10
+python robustness.py --dataset resnet --setting 2 --processes $PROCESSES --num_users 10 --local_ep 10
+python robustness.py --dataset resnet --setting 3 --processes $PROCESSES --num_users 10 --local_ep 10
 
 echo "All runs completed."
 
