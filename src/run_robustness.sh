92584decd5bc2f2d2d46d074603693e2d46af2a5
vincecloutier <vincentcloutier@mail.com>
2024-12-08 08:03:55 +0100

    update script to take dataset

--- src/run_robustness.sh
+++ src/run_robustness.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# get processes and shapley_processes arguments
+# get processes and shapley_processes and dataset arguments
 if [ -z "$1" ]; then
-    PROCESSES=8  # default is eight
+    PROCESSES=8 
 else
     PROCESSES=$1
 fi
 
 if [ -z "$2" ]; then
-    SHAPLEY_PROCESSES=6  # default is six
+    SHAPLEY_PROCESSES=6  
 else
     SHAPLEY_PROCESSES=$2
 fi
 
+if [ -z "$3" ]; then
+    DATASET=cifar
+else
+    DATASET=$3
+fi
+
 echo "Using $PROCESSES processes."
 
-python robustness.py --dataset resnet --setting 0 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset resnet --setting 1 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset resnet --setting 2 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset resnet --setting 3 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 0 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 1 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 2 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 3 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
 
 echo "All runs completed."
 
 # chmod +x run_robustness.sh
-# ./run_robustness.sh [processes] [shapley_processes]
+# ./run_robustness.sh [processes] [shapley_processes] [dataset]
