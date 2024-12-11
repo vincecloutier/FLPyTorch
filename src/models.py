8b0ab1c6e6e8c6c5bdc524de7ab88407028ca276
vincecloutier <vincentcloutier@mail.com>
2024-12-11 09:47:42 +0100

    trying a simpler fmnist model

--- src/models.py
+++ src/models.py
@@ -2,19 +2,43 @@ from torch import nn
 import torch.nn.functional as F
 from torchvision.models import resnet50, resnet18
 
+# class CNNFashion(nn.Module):
+#     def __init__(self, args):
+#         super(CNNFashion, self).__init__()
+#         self.features = nn.Sequential(
+#             nn.Conv2d(1,32, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=2, stride=2),
+#             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=2, stride=2)
+#         )
+#         self.classifier = nn.Sequential(
+#             nn.Dropout(),
+#             nn.Linear(64 * 7 * 7, 128),
+#             nn.ReLU(inplace=True),
+#             nn.Linear(128, 10)
+#         )
+
+#     def forward(self, x):
+#         x = self.features(x)
+#         x = x.view(x.size(0), 64 * 7 * 7)
+#         x = self.classifier(x)
+#         return x
+
+
 class CNNFashion(nn.Module):
     def __init__(self, args):
         super(CNNFashion, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(1,32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.classifier = nn.Sequential(
-            nn.Dropout(),
             nn.Linear(64 * 7 * 7, 128),
             nn.ReLU(inplace=True),
             nn.Linear(128, 10)
