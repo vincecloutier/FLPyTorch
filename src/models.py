6d458ac0a01b3e3690dc50aa5b2fcf2ef982cd70
vincecloutier <vincentcloutier@mail.com>
2024-12-11 16:09:45 +0100

    deeper resnet

--- src/models.py
+++ src/models.py
@@ -28,17 +28,60 @@ from torchvision.models import resnet50, resnet18
 
 
 # resnet for FMNIST
-class BasicBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_planes)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)  
-        self.bn2 = nn.BatchNorm2d(out_planes)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != out_planes:
-            self.shortcut = nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False), nn.BatchNorm2d(out_planes))
+# class BasicBlock(nn.Module):
+#     def __init__(self, in_planes, out_planes, stride=1):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(out_planes)
+#         self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)  
+#         self.bn2 = nn.BatchNorm2d(out_planes)
+#         self.shortcut = nn.Sequential()
+#         if stride != 1 or in_planes != out_planes:
+#             self.shortcut = nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False), nn.BatchNorm2d(out_planes))
 
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.bn2(self.conv2(out))
+#         out += self.shortcut(x)
+#         out = F.relu(out)
+#         return out
+
+# class CNNFashion(nn.Module):
+#     def __init__(self, args, num_classes=10):
+#         super(CNNFashion, self).__init__()
+#         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(16)
+#         self.layer1 = self._make_layer(16, 16, stride=1)
+#         self.layer2 = self._make_layer(16, 32, stride=2)
+#         self.layer3 = self._make_layer(32, 64, stride=2)
+#         self.linear = nn.Linear(64, num_classes)
+
+#     def _make_layer(self, in_planes, out_planes, stride):
+#         return nn.Sequential(BasicBlock(in_planes, out_planes, stride=stride))
+
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x))) 
+#         out = self.layer1(out)
+#         out = self.layer2(out)
+#         out = self.layer3(out)  
+#         out = F.avg_pool2d(out, out.size(2))
+#         out = out.view(out.size(0), -1)
+#         out = self.linear(out)
+#         return out
+
+
+class BasicBlock(nn.Module):
+    expansion = 1 
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False), nn.BatchNorm2d(self.expansion*planes))
+    
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
@@ -47,27 +90,36 @@ class BasicBlock(nn.Module):
         return out
 
 class CNNFashion(nn.Module):
-    def __init__(self, args, num_classes=10):
+    def __init__(self, args, block=BasicBlock, num_blocks=[3, 4, 6, 3], num_classes=10):
         super(CNNFashion, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(16, 16, stride=1)
-        self.layer2 = self._make_layer(16, 32, stride=2)
-        self.layer3 = self._make_layer(32, 64, stride=2)
-        self.linear = nn.Linear(64, num_classes)
-
-    def _make_layer(self, in_planes, out_planes, stride):
-        return nn.Sequential(BasicBlock(in_planes, out_planes, stride=stride))
-
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False) 
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64,  num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+    
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+    
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x))) 
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)  
-        out = F.avg_pool2d(out, out.size(2))
+        out = self.layer1(out)                 
+        out = self.layer2(out)                 
+        out = self.layer3(out)                 
+        out = self.layer4(out)                 
+        out = F.avg_pool2d(out, out.size()[3])  
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
+
 
 
 class CNNCifar(nn.Module):
