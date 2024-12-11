bc87a830f33d9f22a88e6c704f762499cb1519ce
vincecloutier <vincentcloutier@mail.com>
2024-12-11 13:23:40 +0100

    passing in local epochs

--- src/run_robustness.sh
+++ src/run_robustness.sh
@@ -19,14 +19,20 @@ else
     DATASET=$3
 fi
 
+if [ -z "$4" ]; then
+    LOCAL_EP=10
+else
+    LOCAL_EP=$4
+fi
+
 echo "Using $PROCESSES processes."
 
-python robustness.py --dataset $DATASET --setting 0 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset $DATASET --setting 1 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset $DATASET --setting 2 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
-python robustness.py --dataset $DATASET --setting 3 --processes $PROCESSES --num_users 10 --local_ep 10 --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 0 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 1 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 2 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
+python robustness.py --dataset $DATASET --setting 3 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES
 
 echo "All runs completed."
 
 # chmod +x run_robustness.sh
-# ./run_robustness.sh [processes] [shapley_processes] [dataset]
+# ./run_robustness.sh [processes] [shapley_processes] [dataset] [local_ep]
