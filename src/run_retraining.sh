b931c13b3f02e6c28bf716efd6afb4fd30423564
vincecloutier <vincentcloutier@mail.com>
2024-12-11 14:46:12 +0100

    trying something else

--- src/run_retraining.sh
+++ src/run_retraining.sh
@@ -27,7 +27,7 @@ do
     # python retraining.py --dataset resnet --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.6
 
     # python retraining.py --dataset fmnist --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --num_categories_per_client 4 --acc_stopping 0
-    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.2 --acc_stopping 0 --frac 0.2
+    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.2 --acc_stopping 0 --local_bs 32
     # python retraining.py --dataset fmnist --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.6 --acc_stopping 0
 
     # python retraining.py --dataset resnet --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --num_categories_per_client 6
@@ -35,7 +35,7 @@ do
     # python retraining.py --dataset resnet --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.4
 
     # python retraining.py --dataset fmnist --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --num_categories_per_client 6 --acc_stopping 0
-    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.4 --acc_stopping 0 --frac 0.2
+    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.4 --acc_stopping 0 --local_bs 32
     # python retraining.py --dataset fmnist --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.4 --acc_stopping 0
 
     # python retraining.py --dataset resnet --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --num_categories_per_client 8
@@ -43,7 +43,7 @@ do
     # python retraining.py --dataset resnet --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.2
 
     # python retraining.py --dataset fmnist --setting 1 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --num_categories_per_client 8 --acc_stopping 0
-    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.6 --acc_stopping 0 --frac 0.2
+    python retraining.py --dataset fmnist --setting 2 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.6 --acc_stopping 0  --local_bs 32
     # python retraining.py --dataset fmnist --setting 3 --processes $PROCESSES --badclient_prop $BAD_CLIENT_PROP --num_users 10 --local_ep 10 --retrain 1 --badsample_prop 0.2 --acc_stopping 0
 
 done
