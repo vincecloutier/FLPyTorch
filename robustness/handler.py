ee4facdd616f403f0e164b8ff7e93871c65c204f
vincecloutier <vincentcloutier@mail.com>
2024-12-12 16:16:05 +0100

    collecting both data sets

--- robustness/handler.py
+++ robustness/handler.py
@@ -102,3 +102,4 @@ def process_and_graph_logs(log_files, plot=False):
         plt.savefig(f"robustness/graphs/robustness_{dataset}.png", dpi=300, bbox_inches='tight')
 
 process_and_graph_logs(['robustness/cifar0.log', 'robustness/cifar1.log', 'robustness/cifar2.log', 'robustness/cifar3.log'], plot=True)
+process_and_graph_logs(['robustness/fmnist0.log', 'robustness/fmnist1.log', 'robustness/fmnist2.log', 'robustness/fmnist3.log'], plot=True)
