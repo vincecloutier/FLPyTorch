fe5e1a518956a0c465c3f268eb442544d4c12868
vincecloutier <vincentcloutier@mail.com>
2024-12-12 15:39:56 +0100

    explicit influence parameter

--- src/valuation/influence.py
+++ src/valuation/influence.py
@@ -27,7 +27,6 @@ class ClassificationTask(Task):
         return F.cross_entropy(logits, sampled_labels, reduction="sum")
 
     def compute_measurement(self, batch: BATCH_TYPE, model: nn.Module) -> torch.Tensor:
-        # Copied from: https://github.com/MadryLab/trak/blob/main/trak/modelout_functions.py.
         inputs, labels = batch
         logits = model(inputs)
 
@@ -59,18 +58,36 @@ def compute_influence(args, global_weights, train_dataset, test_dataset, user_gr
     # define task and prepare model
     task = ClassificationTask()
     model = prepare_model(model, task)
-    analyzer = Analyzer(analysis_name="cifar10", model=model, task=task, profile=False)
+    analyzer = Analyzer(analysis_name="analysis", model=model, task=task, profile=False)
 
     # configure parameters
     dataloader_kwargs = DataLoaderKwargs(num_workers=4)
     analyzer.set_dataloader_kwargs(dataloader_kwargs)
 
     # compute influence factors
-    factors_name = "ekfac"
-    factor_args = FactorArguments(strategy=factors_name)
-    # use half precision
-    # factor_args = all_low_precision_factor_arguments(strategy=factors_name, dtype=torch.bfloat16)
-    # factors_name += "_half"
+    factor_args = FactorArguments(
+        strategy=factors_name,
+        use_empirical_fisher=True,
+        amp_dtype=None,
+        amp_scale=2.0**16,
+
+        covariance_max_examples=100_000,
+        covariance_data_partitions=1,
+        covariance_module_partitions=1,
+        activation_covariance_dtype=torch.float16,
+        gradient_covariance_dtype=torch.float16,
+
+        eigendecomposition_dtype=torch.float16, 
+        lambda_max_examples=100_000,
+        lambda_data_partitions=1,
+        lambda_module_partitions=1,
+        use_iterative_lambda_aggregation=False,
+        offload_activations_to_cpu=False,
+        per_sample_gradient_dtype=torch.float16,  
+        lambda_dtype=torch.float16,
+    )
+    factors_name = factor_args.strategy
+
     # fit all factors
     analyzer.fit_all_factors(
         factors_name=factors_name,
@@ -81,22 +98,42 @@ def compute_influence(args, global_weights, train_dataset, test_dataset, user_gr
     )
 
     # compute pairwise scores
-    score_args = ScoreArguments()
+    score_args = ScoreArguments(
+        damping_factor=1e-10,
+        amp_dtype=None,
+        offload_activations_to_cpu=False,
+
+        data_partitions=1,
+        module_partitions=1,
+        compute_per_module_scores=False,
+        compute_per_token_scores=False,
+        use_measurement_for_self_influence=False,
+        aggregate_query_gradients=False,
+        aggregate_train_gradients=False,
+
+        query_gradient_low_rank=None,
+        use_full_svd=False,
+        query_gradient_svd_dtype=torch.float16,
+        query_gradient_accumulation_steps=1,
+        
+        score_dtype=torch.float16,
+        per_sample_gradient_dtype=torch.float16,
+        precondition_dtype=torch.float16,
+    )
     scores_name = factor_args.strategy
-    # use half precision
-    # score_args = all_low_precision_score_arguments(dtype=torch.bfloat16)
-    # scores_name += "_half"
-    # compute scores
+
+    # compute 
     analyzer.compute_pairwise_scores(
         scores_name=scores_name,
         score_args=score_args,
         factors_name=factors_name,
         query_dataset=test_dataset,
-        query_indices=list(range(1000)),
+        query_indices=list(range(2000)),
         train_dataset=t_dataset,
         per_device_query_batch_size=512,
         overwrite_output_dir=True
     )
+
     scores = analyzer.load_pairwise_scores(scores_name)["all_modules"]
     print(f"Scores shape: {scores.shape}")
 
@@ -104,7 +141,6 @@ def compute_influence(args, global_weights, train_dataset, test_dataset, user_gr
     
     # sum influence scores over all test samples for each training sample -> shape: [num_train_samples]
     influence_scores = scores.sum(dim=0)
-    influence_scores = influence_scores / influence_scores.max()  # normalize
     
     # iterate over each client and sum the influence scores of their training samples
     for client_id, sample_indices in user_groups.items():
