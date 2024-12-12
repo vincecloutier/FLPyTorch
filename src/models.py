fda057d7d3cb8441f3613c4191ce26777dbbeed9
vincecloutier <vincentcloutier@mail.com>
2024-12-12 16:15:48 +0100

    old cnn fashion

--- src/models.py
+++ src/models.py
@@ -3,29 +3,29 @@ import torch.nn.functional as F
 from torchvision.models import resnet50, resnet18
 import torch
 
-# class CNNFashion(nn.Module):
-#     def __init__(self, args):
-#         super(CNNFashion, self).__init__()
-#         self.features = nn.Sequential(
-#             nn.Conv2d(1,32, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2),
-#             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2)
-#         )
-#         self.classifier = nn.Sequential(
-#             nn.Dropout(),
-#             nn.Linear(64 * 7 * 7, 128),
-#             nn.ReLU(inplace=True),
-#             nn.Linear(128, 10)
-#         )
+class CNNFashion(nn.Module):
+    def __init__(self, args):
+        super(CNNFashion, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1,32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(64 * 7 * 7, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 10)
+        )
 
-#     def forward(self, x):
-#         x = self.features(x)
-#         x = x.view(x.size(0), 64 * 7 * 7)
-#         x = self.classifier(x)
-#         return x
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 64 * 7 * 7)
+        x = self.classifier(x)
+        return x
 
 
 # resnet for FMNIST
@@ -71,27 +71,27 @@ import torch
 #         return out
 
 
-class CNNFashion(nn.Module):
-    def __init__(self, args):
-        super(CNNFashion, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, 10)
+# class CNNFashion(nn.Module):
+#     def __init__(self, args):
+#         super(CNNFashion, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+#         self.bn1 = nn.BatchNorm2d(32)
+#         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+#         self.bn2 = nn.BatchNorm2d(64)
+#         self.fc1 = nn.Linear(64 * 7 * 7, 128)
+#         self.dropout1 = nn.Dropout(0.5)
+#         self.fc2 = nn.Linear(128, 10)
         
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.max_pool2d(x, 2)
-        x = x.view(-1, 64 * 7 * 7)
-        x = F.relu(self.fc1(x))
-        x = self.dropout1(x)
-        x = self.fc2(x)
-        return x
+#     def forward(self, x):
+#         x = F.relu(self.bn1(self.conv1(x)))
+#         x = F.max_pool2d(x, 2)
+#         x = F.relu(self.bn2(self.conv2(x)))
+#         x = F.max_pool2d(x, 2)
+#         x = x.view(-1, 64 * 7 * 7)
+#         x = F.relu(self.fc1(x))
+#         x = self.dropout1(x)
+#         x = self.fc2(x)
+#         return x
 
 class CNNCifar(nn.Module):
     def __init__(self, args):
