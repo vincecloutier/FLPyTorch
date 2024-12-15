f1614d14e7dc3e03af991a46d4419afcfa8c79c9
vincecloutier <vincentcloutier@mail.com>
2024-12-15 16:30:40 +0100

    passing in LR to robustness

--- src/run_robustness.sh
+++ src/run_robustness.sh
@@ -31,14 +31,20 @@ else
     STRATEGY=$5
 fi
 
+if [ -z "$6" ]; then
+    LR=0.01
+else
+    LR=$6
+fi
+
 echo "Using $PROCESSES processes."
 
-python robustness.py --dataset $DATASET --setting 0 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY
-python robustness.py --dataset $DATASET --setting 1 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY
-python robustness.py --dataset $DATASET --setting 2 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY
-python robustness.py --dataset $DATASET --setting 3 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY
+python robustness.py --dataset $DATASET --setting 0 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY --lr $LR
+python robustness.py --dataset $DATASET --setting 1 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY --lr $LR
+python robustness.py --dataset $DATASET --setting 2 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY --lr $LR
+python robustness.py --dataset $DATASET --setting 3 --processes $PROCESSES --num_users 10 --local_ep $LOCAL_EP --epochs 15 --shapley_processes $SHAPLEY_PROCESSES --strategy $STRATEGY --lr $LR
 
 echo "All runs completed."
 
 # chmod +x run_robustness.sh
-# ./run_robustness.sh [processes] [shapley_processes] [dataset] [local_ep] [strategy]
+# ./run_robustness.sh [processes] [shapley_processes] [dataset] [local_ep] [strategy] [lr]
