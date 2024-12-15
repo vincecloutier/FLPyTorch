f41f61b196b0f7dde2da55ad6ddeb13652035bf0
vincecloutier <vincentcloutier@mail.com>
2024-12-15 11:45:25 +0100

    manual score arguments

--- src/valuation/influence.py
+++ src/valuation/influence.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from kronfluence.analyzer import Analyzer, prepare_model
-from kronfluence.arguments import FactorArguments
+from kronfluence.arguments import FactorArguments, ScoreArguments
 from kronfluence.task import Task
 from kronfluence.utils.dataset import DataLoaderKwargs
 from utils import get_device, initialize_model
@@ -34,7 +34,7 @@ class ClassificationTask(Task):
         cloned_logits = logits.clone()
         cloned_logits[bindex, labels] = torch.tensor(-torch.inf, device=logits.device, dtype=logits.dtype)
 
-        margins = torch.exp(logits_correct) / torch.exp(cloned_logits).sum(dim=-1)
+        margins = logits_correct - cloned_logits.logsumexp(dim=-1)
         return -margins.sum()
 
 
@@ -63,7 +63,32 @@ def compute_influence(args, global_weights, train_dataset, user_groups, noise_tr
     analyzer.set_dataloader_kwargs(dataloader_kwargs)
 
     # compute influence factors
-    factor_args = FactorArguments(strategy=args.strategy)
+    factor_args = FactorArguments(
+        strategy=args.strategy,
+        use_empirical_fisher=True,
+        amp_dtype=None,
+        amp_scale=2.0**16,
+        has_shared_parameters=False,
+
+        # settings for fitting covariance matrix
+        covariance_max_examples=25_000,
+        covariance_data_partitions=1,
+        covariance_module_partitions=1,
+        activation_covariance_dtype=torch.float16,
+        gradient_covariance_dtype=torch.float16,
+        
+        # settings for eigendecomposition
+        eigendecomposition_dtype=torch.float16,
+        
+        # settings for fitting lambda matrix
+        lambda_max_examples=25_000,
+        lambda_data_partitions=1,
+        lambda_module_partitions=1,
+        use_iterative_lambda_aggregation=False,
+        offload_activations_to_cpu=False,
+        per_sample_gradient_dtype=torch.float16,
+        lambda_dtype=torch.float16,
+    )
     analyzer.fit_all_factors(
         factors_name=args.strategy,
         dataset=t_dataset,
@@ -72,17 +97,44 @@ def compute_influence(args, global_weights, train_dataset, user_groups, noise_tr
         overwrite_output_dir=True,
     )
 
-    # compute self-influence scores
+    # compute influence scores
+    score_args = ScoreArguments(
+        damping_factor=1e-04,
+        amp_dtype=None,
+        offload_activations_to_cpu=False,
+
+        # more settings to compute influence scores
+        data_partitions=1,
+        module_partitions=1,
+        compute_per_module_scores=False,
+        compute_per_token_scores=False,
+        use_measurement_for_self_influence=False,
+        aggregate_query_gradients=False,
+        aggregate_train_gradients=False,
+
+        # settings for query batching
+        query_gradient_low_rank=None,
+        use_full_svd=False,
+        query_gradient_svd_dtype=torch.float16,
+        query_gradient_accumulation_steps=1,
+        
+        # settings for dtype
+        score_dtype=torch.float16,
+        per_sample_gradient_dtype=torch.float16,
+        precondition_dtype=torch.float16,
+    )
+
     analyzer.compute_self_scores(
         scores_name=args.strategy,
         factors_name=args.strategy,
         train_dataset=t_dataset,
+        score_args=score_args,
         overwrite_output_dir=True,
     )
+
+    # load scores
     scores = analyzer.load_self_scores(args.strategy)["all_modules"]
-
     print(f"Scores shape: {scores.shape}")
-
     client_influences = defaultdict(float)
     for client_id, sample_indices in user_groups.items():
         sample_indices = torch.tensor(list(sample_indices), dtype=torch.long, device=scores.device)
