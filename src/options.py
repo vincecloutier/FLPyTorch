1724042c68d413495b436550117723020ce64b57
vincecloutier <vincentcloutier@mail.com>
2024-12-15 13:05:40 +0100

    writing

--- src/options.py
+++ src/options.py
@@ -20,7 +20,7 @@ def args_parser():
     # training arguments
     parser.add_argument('--local_ep', type=int, default=10, help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=128, help="local batch size: B")
-    parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+    parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
     parser.add_argument('--weight_decay', type=float, default=1e-4, help='weight decay')
     parser.add_argument('--acc_stopping', type=int, default=1, help='Stopping criterion based on accuracy (default: 1)')
 
