8eacb1611ec282a000799cdd9c06ffe079361afd
vincecloutier <vincentcloutier@mail.com>
2024-12-15 12:47:36 +0100

    restored CNN Fashion and added CNNFashion2

--- src/models.py
+++ src/models.py
@@ -15,7 +15,7 @@ class CNNFashion(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.classifier = nn.Sequential(
-            # nn.Dropout(),
+            nn.Dropout(),
             nn.Linear(64 * 7 * 7, 128),
             nn.ReLU(inplace=True),
             nn.Linear(128, 10)
@@ -26,6 +26,38 @@ class CNNFashion(nn.Module):
         x = x.view(x.size(0), 64 * 7 * 7)
         x = self.classifier(x)
         return x
+
+
+class FashionCNN(nn.Module):
+    def __init__(self, args):
+        super(FashionCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
 
 
 class CNNCifar(nn.Module):
