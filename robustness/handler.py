262b95f62620aab51f4717cc21be951a03179952
vincecloutier <vincentcloutier@mail.com>
2024-12-16 11:34:00 +0100

    fmnist

--- robustness/handler.py
+++ robustness/handler.py
@@ -60,7 +60,7 @@ def process_and_graph_logs(log_files, plot=False):
         approx_simple, approx_hessian, shapley, influence, runtimes = process_log(log_file)
         corr_metrics['FBVS'].append(compute_rank_stability(approx_simple))
         corr_metrics['FBVH'].append(compute_rank_stability(approx_hessian))
-        # corr_metrics['FSV'].append(compute_rank_stability(shapley))
+        corr_metrics['FSV'].append(compute_rank_stability(shapley))
         corr_metrics['Influence'].append(compute_rank_stability(influence))
         for run in runtimes:
             runtime_metrics["FBVS"].append(run["abvs"])
@@ -103,6 +103,5 @@ def process_and_graph_logs(log_files, plot=False):
 
         plt.savefig(f"robustness/graphs/robustness_{dataset}.png", dpi=300, bbox_inches='tight')
 
-process_and_graph_logs(['robustness/cifar1.log', 'robustness/cifar2.log'])
 # process_and_graph_logs(['robustness/cifar0.log', 'robustness/cifar1.log', 'robustness/cifar2.log', 'robustness/cifar3.log'], plot=True)
-# process_and_graph_logs(['robustness/fmnist0.log', 'robustness/fmnist1.log', 'robustness/fmnist2.log', 'robustness/fmnist3.log'], plot=True)
+process_and_graph_logs(['robustness/fmnist0.log', 'robustness/fmnist1.log', 'robustness/fmnist2.log', 'robustness/fmnist3.log'], plot=True)
